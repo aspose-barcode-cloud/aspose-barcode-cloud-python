1adef4fe8356042a7c17c80799a0f93a39b06f3a
Denis Averin <denis.averin@aspose.com>
2020-06-01 19:54:50 +0700

    Fixed upload script

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,13 +3,15 @@ import unittest
 
 from aspose_barcode_cloud import Configuration, ApiClient, BarcodeApi, EncodeBarcodeType
 
+CONFIG_FILENAME = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'configuration.qa.json')
 
+
+@unittest.skipUnless(os.path.exists(CONFIG_FILENAME), "Requested file '%s' does not exist" % CONFIG_FILENAME)
 class TestAuth(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.config = Configuration.from_file(os.path.join(os.path.split(os.path.abspath(__file__))[0],
-                                                          'configuration.json'))
+        cls.config = Configuration.from_file(CONFIG_FILENAME)
         cls.api_client = ApiClient(cls.config)
         cls.api = BarcodeApi(cls.api_client)
 
