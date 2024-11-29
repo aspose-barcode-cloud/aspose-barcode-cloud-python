2553258ca23f8ec6f16fffb869fd6e694df46feb
ivankamkin <109517020+ivankamkin@users.noreply.github.com>
2024-11-29 15:04:16 +0500

    Version updated (#82)

--- aspose_barcode_cloud/configuration.py
+++ aspose_barcode_cloud/configuration.py
@@ -258,7 +258,7 @@ class Configuration(object):
             "OS: {env}\n"
             "Python Version: {pyversion}\n"
             "Version of the API: 3.0\n"
-            "SDK Package Version: 24.10.0".format(env=sys.platform, pyversion=sys.version)
+            "SDK Package Version: 24.11.0".format(env=sys.platform, pyversion=sys.version)
         )
 
     @staticmethod
