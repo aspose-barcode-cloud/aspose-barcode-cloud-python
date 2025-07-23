ca56680c718627d5e82b75bbb359781d243de986
Denis Averin <59285247+Denis-Averin@users.noreply.github.com>
2025-07-23 13:54:45 +0500

    Update vulnerable versions of dependencies (#92)
    * Update vulnerable versions of dependencies
    
    * Drop old Python 3.7 and 3.8
    
    * Fix check_python_versions script

--- scripts/run_snippet.sh
+++ scripts/run_snippet.sh
@@ -8,6 +8,6 @@ SCRIPT_DIR=$3
 CONFIG_FILE_PATH=$4
 echo "Run snippet file: $FILE_PATH"
 
-python ${SCRIPT_DIR}/insert-credentials.py $FILE_PATH $CONFIG_FILE_PATH $RUN_DIR
+python "${SCRIPT_DIR}/insert-credentials.py" "$FILE_PATH" "$CONFIG_FILE_PATH" "$RUN_DIR"
 
-python ./$RUN_DIR/"${FILE_PATH##*/}" || exit 1
+python "./$RUN_DIR/${FILE_PATH##*/}" || exit 1
